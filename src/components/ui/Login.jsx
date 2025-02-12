1d1323abb9114371797839b178466e0e3b014cde
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-02-12 10:13:15 +0500

    done

--- src/components/ui/Login.jsx
+++ src/components/ui/Login.jsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 import { auth } from "../../../firebase";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
@@ -24,6 +27,9 @@ const Login = () => {
       console.log(`Error in Login to your Account: ${error}`);
       setErrorMessage(error);
     }
+  };
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
   };
   return (
     <div className=" p-5 max-w-md xs:mx-auto mx-5 font-Karla shadow-sm flex flex-col justify-center items-center m-5 gap-3 ">
@@ -52,18 +58,29 @@ const Login = () => {
             className="w-full border border-slate-700 py-2 px-4 rounded-md outline-none focus:ring-3 focus:ring-red-400 transition-all duration-300 focus:border-none"
           />
         </div>
-        <div className="space-y-3">
+        <div className="space-y-3 relative">
           <label htmlFor="password" className="block">
             Password
           </label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             id="password"
             placeholder="Enter your Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="w-full border border-slate-700 py-2 px-4 rounded-md outline-none focus:ring-3 focus:ring-red-400 transition-all duration-300 focus:border-none"
           />
+          {showPassword ? (
+            <BsEyeFill
+              className="w-5 h-5 absolute top-11.5 right-5 text-slate-500 cursor-pointer"
+              onClick={handleShowPassword}
+            />
+          ) : (
+            <BsEyeSlashFill
+              className="w-5 h-5 absolute top-11.5 right-5 text-slate-500 cursor-pointer"
+              onClick={handleShowPassword}
+            />
+          )}
         </div>
 
         <button
--- src/components/ui/SignUp.jsx
+++ src/components/ui/SignUp.jsx
@@ -4,6 +4,8 @@ import { auth } from "../../../firebase";
 import { db } from "../../../firebase";
 import { setDoc, doc } from "firebase/firestore";
 import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 const SignUp = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -11,10 +13,14 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
-
+  const naviagte = useNavigate();
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
+      if (password !== confirmPassword) {
+        setError("Passwords do not match");
+        return;
+      }
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
@@ -30,15 +36,32 @@ const SignUp = () => {
       });
       Swal.fire({
         title: "Congratulations",
-        text: "Your Account is Created Successfully",
+        text: "Your account has been created successfully",
         showConfirmButton: false,
         timer: 1500,
-        icon : "success"
+        icon: "success",
       });
+      naviagte("/");
+      // Clear form fields
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      setError("");
     } catch (error) {
       console.log(error);
       setError(error.message);
     }
+  };
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
   };
   return (
     <div className=" p-5 max-w-md mx-auto font-Karla shadow-sm flex flex-col justify-center items-center m-5 gap-3">
@@ -92,31 +115,53 @@ const SignUp = () => {
             className="w-full border border-slate-700 py-2 px-4 rounded-md outline-none focus:ring-3 focus:ring-red-400 transition-all duration-300 focus:border-none"
           />
         </div>
-        <div className="space-y-3">
+        <div className="space-y-3 relative">
           <label htmlFor="password" className="block">
             Password
           </label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             id="password"
             placeholder="Enter your Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="w-full border border-slate-700 py-2 px-4 rounded-md outline-none focus:ring-3 focus:ring-red-400 transition-all duration-300 focus:border-none"
           />
+          {showPassword ? (
+            <BsEyeFill
+              className="w-5 h-5 absolute top-11.5 right-5 text-slate-500 cursor-pointer"
+              onClick={handleShowPassword}
+            />
+          ) : (
+            <BsEyeSlashFill
+              className="w-5 h-5 absolute top-11.5 right-5 text-slate-500 cursor-pointer"
+              onClick={handleShowPassword}
+            />
+          )}
         </div>
-        <div className="space-y-3">
+        <div className="space-y-3 relative">
           <label htmlFor="confirmpassword" className="block">
             Confirm Password
           </label>
           <input
-            type="password"
+            type={showConfirmPassword ? "text" : "password"}
             id="confirmpassword"
             placeholder="Enter your Confirm Password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             className="w-full border border-slate-700 py-2 px-4 rounded-md outline-none focus:ring-3 focus:ring-red-400 transition-all duration-300 focus:border-none"
           />
+          {showConfirmPassword ? (
+            <BsEyeFill
+              className="w-5 h-5 absolute top-11.5 right-5 text-slate-500 cursor-pointer"
+              onClick={handleShowConfirmPassword}
+            />
+          ) : (
+            <BsEyeSlashFill
+              className="w-5 h-5 absolute top-11.5 right-5 text-slate-500 cursor-pointer"
+              onClick={handleShowConfirmPassword}
+            />
+          )}
         </div>
         <button
           type="submit"
